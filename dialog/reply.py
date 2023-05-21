18648f373e9c9a96526eb068a5b13987a16e925f
BabaAmane <amane88@keio.jp>
2023-05-21 09:11:32 +0900

    修正後の出力も表示

--- dialog/reply.py
+++ dialog/reply.py
@@ -8,7 +8,7 @@ import argparse
 def generate_reply(tokenizer,inp,model, device, num_gen=1):
     input_text = "<s>" + str(inp) + "[SEP]"
     input_ids = tokenizer.encode(input_text, return_tensors='pt').to(device)
-    # 変数の修正が必要
+    # 変数の修正が必要?
     out = model.generate(input_ids, do_sample=True, max_length=128, num_return_sequences=1, top_p=0.95, top_k=50, bad_words_ids=[[1], [5]], no_repeat_ngram_size=3)
 
     print(">", "あなた")
@@ -25,6 +25,41 @@ def generate_reply(tokenizer,inp,model, device, num_gen=1):
         print(sent)
 
 
+def generate_reply_another(tokenizer,inp,model):  
+    input_text = "<s>" + str(inp) + "[SEP]"
+    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+
+    with torch.no_grad():
+        output_ids = model.generate(
+            input_ids.to(model.device),
+            do_sample=True,
+            top_p=0.95,
+            top_k=100,
+            num_return_sequences=1,
+            max_new_tokens=100,
+            bad_words_ids=[[1], [5]],
+            no_repeat_ngram_size=1,
+            pad_token_id=tokenizer.pad_token_id,
+            bos_token_id=tokenizer.bos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+        )
+    output = tokenizer.decode(output_ids.tolist()[0])
+    sent = (
+        output.replace(inp, "")
+        .replace(" ", "")
+        .replace("<s>", "")
+        .replace("</s>", "")
+        .replace("[SEP]", "")
+    )
+
+    tex = ["。", "?", "？", "！", "!", "."]
+    for t in tex:
+        if t in sent:
+            sent = sent.split(t)[0] + t
+        else:
+            sent = sent
+        return sent
+
 
 
 def main(args):
@@ -35,18 +70,19 @@ def main(args):
 
     tokenizer = T5Tokenizer.from_pretrained(pretrain_model)
     tokenizer.do_lower_case = True
-    # model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt-1b")
-
 
     model = AutoModelForCausalLM.from_pretrained(fainching_model)
     model.to(device)
     model.eval()
-    # print(model)
 
     msg = args.input_text
     
-
+    print('変更前')
     generate_reply(tokenizer,msg,model, device)
+    print('変更後')
+    generate_reply_another(tokenizer,msg,model)
+    
+
 
 
 if __name__ == '__main__':
@@ -57,8 +93,6 @@ if __name__ == '__main__':
                         help='fainching model name')
     parser.add_argument('--input_text', type=str, default='彼女ができません。',
                         help='input text')
-    
-    
 
 
     args = parser.parse_args()
