d22aa72b2973b4138613be38651800d9151a2b35
BabaAmane <amane88@keio.jp>
2023-05-22 14:23:06 +0900

    前しょりの追加

--- worry_text_data/extract_negative_text.py
+++ worry_text_data/extract_negative_text.py
@@ -1,37 +1,93 @@
 from asari.api import Sonar
 import pandas as pd
 from tqdm import tqdm
+from transformers import pipeline 
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 def main():
+    # 使うデータの処理
     dialog_df = pd.read_csv('user_text_data/user_text_data_all_sentence.csv')
-
-    sonar = Sonar()
 
     text_list = list(dialog_df['message'])
     num_list = list(dialog_df['session_id'])
-    negative_message_list = []
-    negative_num_list = []
 
+    # # transformer 感情分析
+    tokenizer = AutoTokenizer.from_pretrained("koheiduck/bert-japanese-finetuned-sentiment")
+    model = AutoModelForSequenceClassification.from_pretrained("koheiduck/bert-japanese-finetuned-sentiment")
+   
+    sentiment_analyzer = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+    negative_message_transformer_list = []
+    negative_num_transformer_list = []
+    negative_score_transformer_list = []
+
+    # text_list = text_list[:200]
     for i, t in tqdm(zip(num_list, text_list)):
-        res = sonar.ping(text=t)
-        # print(i, res['top_class'])
-        if res['top_class'] == 'negative':
-            negative_message_list.append(t)
-            negative_num_list.append(i)
-            
-    print(negative_message_list, negative_num_list)
+        if len(t) <= 512:
+            res = sentiment_analyzer(t)
+            # 条件狭めてもいいかも 70以上とか
+            if res[0]['label'] == 'NEGATIVE':
+                negative_message_transformer_list.append(t)
+                negative_num_transformer_list.append(i)
 
+                if res[0]['score'] < 0.5:
+                    negative_score_transformer_list.append(0)
+                elif res[0]['score'] >= 0.5 and res[0]['score'] < 0.6:
+                    negative_score_transformer_list.append(5)
+                elif res[0]['score'] >= 0.6 and res[0]['score'] < 0.7:
+                    negative_score_transformer_list.append(6)
+                elif res[0]['score'] >= 0.7 and res[0]['score'] < 0.8:
+                    negative_score_transformer_list.append(7)
+                elif res[0]['score'] >= 0.8 and res[0]['score'] < 0.9:
+                    negative_score_transformer_list.append(8)
+                else:
+                    negative_score_transformer_list.append(9)
 
-    df = pd.DataFrame(list(zip(negative_num_list, negative_message_list)), columns = ['session_id', 'message'])
-    df.to_csv('user_text_data_negative_judgment/user_text_data_negative_judgment.csv', index=False)
+    # 学習用全体のデータ
+    df_transformer = pd.DataFrame(list(zip(negative_num_transformer_list, negative_message_transformer_list)), columns = ['session_id', 'message'])
+    df_transformer.to_csv('user_text_data_negative_judgment/user_text_data_negative_judgment_transformer.csv', index=False)
 
-
-
-
-
+    # 分析用のデータ
+    df_transformer_analysis = pd.DataFrame(list(zip(negative_num_transformer_list, negative_message_transformer_list, negative_score_transformer_list)), columns = ['session_id', 'message', 'score'])
+    df_transformer_analysis.to_csv('analysis/user_text_data_negative_judgment_transformer_analysis.csv', index=False)
+    
     
 
-    
+    # sonar = Sonar()
+    # negative_message_asari_list = []
+    # negative_num_asari_list = []
+    # negative_score_asari_list = []
+    # print('//////////////asari////////////////')
+    # for i, t in tqdm(zip(num_list, text_list)):
+    #     res = sonar.ping(text=t)
+    #     #条件せば目ても良い
+    #     if res['top_class'] == 'negative':
+    #         negative_message_asari_list.append(t)
+    #         negative_num_asari_list.append(i)
+
+    #         if res['classes'][0]['confidence'] < 0.5:
+    #             negative_score_asari_list.append(0)
+    #         elif res['classes'][0]['confidence'] >= 0.5 and res['classes'][0]['confidence'] < 0.6:
+    #             negative_score_asari_list.append(5)
+    #         elif res['classes'][0]['confidence'] >= 0.6 and res['classes'][0]['confidence'] < 0.7:
+    #             negative_score_asari_list.append(6)
+    #         elif res['classes'][0]['confidence'] >= 0.7 and res['classes'][0]['confidence'] < 0.8:
+    #             negative_score_asari_list.append(7)
+    #         elif res['classes'][0]['confidence'] >= 0.8 and res['classes'][0]['confidence'] < 0.9:
+    #             negative_score_asari_list.append(8)
+    #         else:
+    #             negative_score_asari_list.append(9)
+
+    # # 学習用
+    # df_asari = pd.DataFrame(list(zip(negative_num_asari_list, negative_message_asari_list)), columns = ['session_id', 'message'])
+    # df_asari.to_csv('user_text_data_negative_judgment/user_text_data_negative_judgment_asari.csv', index=False)
+
+    # # 分析用
+    # df_asari_analysis = pd.DataFrame(list(zip(negative_num_asari_list, negative_message_asari_list, negative_score_asari_list)), columns = ['session_id', 'message', 'score'])
+    # df_asari_analysis.to_csv('analysis/user_text_data_negative_judgment_asari.csv', index=False)
+
+
 
 if __name__=='__main__':
     main()
--- worry_text_data/extract_worry_text.py
+++ worry_text_data/extract_worry_text.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+# 余計なデータを削除するファイル
+
+def contains_word_japanese(text: str, drop_word_list: list):
+    # 文章を単語や句読点などで分割し、単語ごとに比較する
+    words = text.split()
+    for w in words:
+        for drop_word in drop_word_list:
+            if drop_word in w:
+                # print('drop_word', drop_word)
+                return True
+    return False
+
 def main():
     # userの余計な文章削除
     dialog_df = pd.read_csv('after_ processing_data/dialog_data_after_process2.csv')
@@ -17,54 +29,52 @@ def main():
                       'ごもっともですね', 'なんでわかるんですか', 'わかりました', 'そうなんですか', 'お願いいたします','お願いします', 'ふむふむなるほど', \
                       'そうなんですね', 'なるほど', '初めましてです', 'ありがとうございました', 'それはないですね', 'そーなんですよ', 'それはないですね', 'これでよいでしょうか', \
                       'なるほど楽しみです', 'おおそうなんですね', 'なるほど大変参考になります', 'はじめまして', 'そうなんですね', 'ええー', 'なんですか',  \
-                      'ほかのも聞きたいです', 'すみません', 'よろしくお願い致します'
+                      'ほかのも聞きたいです', 'すみません', 'よろしくお願い致します', 'ありがとうございます'
                       
                       ]
     drop_spesific_word_list = ['利き手','生年月日','年月日', '見ていただけますか', '手相', '誕生日', '座', '型', '当たっております']
     drop_word_list = drop_reply_list + drop_spesific_word_list
-    print(drop_word_list)
+    # print(drop_word_list)
 
-    # めかぶかなんかで人の名前だった場合削除
 
     for messsage in user_text_df['message']:
         if len(messsage) > 7:
-            i = 0
-            for drop_word in drop_word_list:
-                index = messsage.find(drop_word)
-                if index != -1:
-                    # あまりにも長すぎる場合は悩みの可能性が高いので処理が必要
-                    if len(messsage) > 30:
-                        drop_list.append(0)
-                        break
-                    else:
-                        drop_list.append(1)
-                    break
-                if i == len(drop_word_list) - 1 :
-                    drop_list.append(0)
-                    break
-                i += 1
+            judgment = contains_word_japanese(messsage, drop_word_list)
+            if judgment == True:
+                drop_list.append(1)
+                # print('除去対象悩みの文章')
+                # print(messsage)
+            else:
+                drop_list.append(0)
+    
         else:
             drop_list.append(1)
     
+    print(len(user_text_df))
+    print(len(drop_list))
+
+    session_id_list = list(user_text_df['session_id'])
+    message_list = list(user_text_df['message'])
+
+    df_result = pd.DataFrame(list(zip(session_id_list, message_list, drop_list)), columns = ['session_id', 'message', 'drop_flag'])
+    
     # データフレームにカラムを追加
-    user_text_df['drop_flag'] = drop_list
+    # user_text_df['drop_flag'] = drop_list
+    # user_text_df.loc[:, 'drop_flag'] = drop_list
 
     # dropが1のデータは削除
-    user_text_df = user_text_df[user_text_df['drop_flag'] == 0]
-
-    # いらないカラム削除
-    user_text_df.drop(['message_type', 'user_id', 'counsellor_id', 'drop_flag'], axis=1, inplace=True)
+    # user_text_df = user_text_df[user_text_df['drop_flag'] == 0]
+    df_result = df_result[df_result['drop_flag'] == 0]
+    # いらないカラム削除s
+    # user_text_df.drop(['message_type', 'user_id', 'counsellor_id', 'drop_flag'], axis=1, inplace=True)
     # 前処理を一旦終えたファイル
-    user_text_df.to_csv('user_text_data/user_text_data_all_sentence.csv', index=False)
-
+    # user_text_df.to_csv('user_text_data/user_text_data_all_sentence.csv', index=False)
+    df_result.to_csv('user_text_data/user_text_data_all_sentence.csv', index=False)
 
     ## 会話の一番初めのデータだけを取得
-    # session_idの取得
-    # session_id_list = user_text_df['session_id'].to_list()
-    # session_id_list = sorted(set(session_id_list), key=session_id_list.index)
-
-    user_text_df.drop_duplicates(subset='session_id', keep='first', inplace=True)
-    user_text_df.to_csv('user_text_data/user_text_data_one_sentence.csv', index=False)
+    
+    # user_text_df.drop_duplicates(subset='session_id', keep='first', inplace=True)
+    # user_text_df.to_csv('user_text_data/user_text_data_one_sentence.csv', index=False)
 
    
 
