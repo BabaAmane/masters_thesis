11a03fd62e264af95c956bd25eef126f64e49531
BabaAmane <amane88@keio.jp>
2023-08-05 12:07:37 +0900

    add val

--- dialog/reply.py
+++ dialog/reply.py
@@ -1,9 +1,10 @@
 from transformers import T5Tokenizer, AutoModelForCausalLM
 import torch
 from pathlib import Path
-
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import argparse
+import pandas as pd
+from tqdm import tqdm
 
 def generate_reply(tokenizer,inp,model, device, num_gen=1):
     input_text = "<s>" + str(inp) + "[SEP]"
@@ -11,64 +12,31 @@ def generate_reply(tokenizer,inp,model, device, num_gen=1):
     # 変数の修正が必要?
     out = model.generate(input_ids, 
                          do_sample=True, 
-                         max_length=30, 
+                         max_length=40, 
                          num_return_sequences=1, 
                          top_p=0.95, 
                          top_k=50, 
                          bad_words_ids=[[1], [5]], 
                          no_repeat_ngram_size=3)
 
-    print(">", "あなた")
-    print(inp)
-    print(">", "対話システム")
-    # print("out", out)
-    # print("新規", tokenizer.decode(out.tolist()[0]))
-    # print(tokenizer.batch_decode(out))
+    # print(">", "あなた")
+    # print(inp)
+    # print(">", "対話システム")
+    
+    encouragement_text = []
 
     for sent in tokenizer.batch_decode(out):
         sent = sent.split('[SEP]</s>')[1]
         sent = sent.replace('</s>', '')
         sent = sent.replace('<br>', '\n')
-        print(sent)
+        # print(sent)
+        encouragement_text.append(sent)
+    
+    return encouragement_text
 
-
-def generate_reply_another(tokenizer,inp,model):  
-    input_text = "<s>" + str(inp) + "[SEP]"
-    input_ids = tokenizer.encode(input_text, return_tensors="pt")
-
-    with torch.no_grad():
-        output_ids = model.generate(
-            input_ids.to(model.device),
-            do_sample=True,
-            max_length=30, 
-            top_p=0.95,
-            top_k=100,
-            num_return_sequences=1,
-            max_new_tokens=100,
-            bad_words_ids=[[1], [5]],
-            no_repeat_ngram_size=1,
-            pad_token_id=tokenizer.pad_token_id,
-            bos_token_id=tokenizer.bos_token_id,
-            eos_token_id=tokenizer.eos_token_id,
-        )
-    output = tokenizer.decode(output_ids.tolist()[0])
-    sent = (
-        output.replace(inp, "")
-        .replace(" ", "")
-        .replace("<s>", "")
-        .replace("</s>", "")
-        .replace("[SEP]", "")
-    )
-
-    # print('output', output)
-    tex = ["。", "?", "？", "！", "!", "."]
-    for t in tex:
-        if t in sent:
-            sent = sent.split(t)[0] + t
-        else:
-            sent = sent
-        return sent
-
+    
+def remove_character_from_list(lst, character):
+    return [item.replace(character, '') for item in lst]
 
 
 def main(args):
@@ -84,26 +52,52 @@ def main(args):
     model.eval()
 
     msg = args.input_text
-    text_list = ['<s>後悔ばかりです。</s>', '<s>彼氏ができません。</s>', '<s>自分の恋愛運を信じれません。</s>', '<s>元カレに裏切られたのでこわいです。</s>', '<s>このまま諦めるのは嫌です。</s>', '<s>切なくて胸が避けそうです。</s>', '<s>ショックのあまり立ち直れないです。</s>', '<s>孤独になるのが怖い。</s>', '<s>就活の終わりが見えていない。</s>', '<s>意気消沈気味です。</s>', '<s>諦めた方がいいですか。</s>']
-    
-    # print('変更前')
-    for text in text_list:
-        generate_reply(tokenizer,text,model, device)
-    # print('変更後')
-    # output = generate_reply_another(tokenizer,msg,model)
-    # print(output)
-    
+    # input_text_list = ['<s>後悔ばかりです。</s>', '<s>彼氏ができません。</s>', '<s>自分の恋愛運を信じれません。</s>', '<s>元カレに裏切られたのでこわいです。</s>', '<s>このまま諦めるのは嫌です。</s>', '<s>切なくて胸が避けそうです。</s>', '<s>ショックのあまり立ち直れないです。</s>', '<s>孤独になるのが怖い。</s>', '<s>就活の終わりが見えていない。</s>', '<s>意気消沈気味です。</s>', '<s>諦めた方がいいですか。</s>']
 
+    # 評価データの読み込み
+    rate = args.rate
+    random_state = args.random_state
+    val_dir = '../train_val/doc2vec/val_' + str(rate) + '_' + str(random_state) + '.txt'
+    lines = []
+    with open(val_dir, 'r', encoding='utf-8') as file:
+        for line in file:
+            lines.append(line.strip())
+    input_text_list = []
+
+    for item in lines:
+        text_up_to_sep = item.split('[SEP]')[0]
+        input_text_list.append(text_up_to_sep)
+
+    # 可視化
+    encouragement_text_list = []
+    for text in tqdm(input_text_list):
+        encouragement_text = generate_reply(tokenizer,text,model, device)
+        encouragement_text_list.append(encouragement_text)
+    
+    # <s>, </s>の削除
+    input_text_list = remove_character_from_list(input_text_list, '<s>')
+    input_text_list = remove_character_from_list(input_text_list, '</s>')
+
+    encouragement_text_list = remove_character_from_list(encouragement_text_list, '<s>')
+    encouragement_text_list = remove_character_from_list(encouragement_text_list, '</s>')
+
+    df_result = pd.DataFrame(list(zip(input_text_list, encouragement_text_list)), columns = ['input_text', 'encouragement_text'])
+    result_dir = args.result_dir
+    df_result.to_csv(result_dir, index=False)
+    
+    
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pretrain_model', type=str, default='rinna/japanese-gpt2-small',
+    parser.add_argument('--pretrain_model', type=str, default='rinna/japanese-gpt2-medium',
                         help='pretrain model name')
-    parser.add_argument('--fainching_model', type=str, default='model_negative_20epoch/',
+    parser.add_argument('--fainching_model', type=str, default='model_0epoch/',
                         help='fainching model name')
-    parser.add_argument('--input_text', type=str, default='彼女ができません。',
-                        help='input text')
+    parser.add_argument('--result_dir', type=str, default='reply_result/result_82_42_40epoch.csv',
+                        help='result_dir')
+    
+    
 
 
     args = parser.parse_args()
